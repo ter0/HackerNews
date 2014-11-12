534ce7b510510e99c9e53ab239d098f7ab297ff0
Chris Johns <cajohns.93@gmail.com>
2014-11-12 14:36:06 +0000

    Add Firebase SDK

--- app/src/main/java/com/teamxx/groupmad/MainActivity.java
+++ app/src/main/java/com/teamxx/groupmad/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
     }
 
 
