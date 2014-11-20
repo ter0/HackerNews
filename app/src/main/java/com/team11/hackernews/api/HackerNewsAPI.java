8f5585c5ac2514c963760f4f8a092b22b2825f74
Chris Johns <cajohns.93@gmail.com>
2014-11-20 14:40:51 +0000

    Remove some unused code

--- app/src/main/java/com/team11/hackernews/api/HackerNewsAPI.java
+++ app/src/main/java/com/team11/hackernews/api/HackerNewsAPI.java
@@ -1,20 +1,10 @@
 package com.team11.hackernews.api;
 
-import android.content.Context;
-
-import com.firebase.client.Firebase;
-
 public class HackerNewsAPI {
     public static final String ROOT_PATH = "https://hacker-news.firebaseio.com/v0";
     public static final String USER = "/user";
     public static final String ITEM = "/item";
     public static final String TOP_STORIES = "/topstories";
-    public static Firebase mFirebase;
-
-    public static void init(Context context) {
-        Firebase.setAndroidContext(context);
-        mFirebase = new Firebase(ROOT_PATH);
-    }
 
     public static String getItemPathById(int id) {
         return ROOT_PATH + ITEM + "/" + id;
