fbdee9230a9a78030c125e42908ae2058c99ea0b
Mehow <Kaiser.mehow@gmail.com>
2014-11-26 15:02:56 +0000

    Adding a basic fragment and activity to show webview

---
+++ app/src/main/java/com/team11/hackernews/WebViewFragment.java
@@ -0,0 +1,98 @@
+package com.team11.hackernews;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link WebViewFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link WebViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WebViewFragment extends Fragment {
+
+    public static final String URL_EXTRA = "URL_EXTRA";
+
+    private static final String ARG_URL = "ARG_URL";
+
+    private String mUrl;
+
+    private OnFragmentInteractionListener mListener;
+
+    public WebViewFragment() {
+        // Required empty public constructor
+    }
+
+    public static WebViewFragment newInstance(String url) {
+        WebViewFragment fragment = new WebViewFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_URL, url);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mUrl = getArguments().getString(ARG_URL);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_web_view, container, false);
+        WebView webView = (WebView) rootView.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(mUrl);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+    }
+
+}
