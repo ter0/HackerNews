73e87bbf381c7c393ef5d264fe16dde028927be9
Tommy Kwan <tkwan196@gmail.com>
2014-12-02 13:40:17 +0000

    Added accessor for poll options

---
+++ app/src/main/java/com/team11/hackernews/api/PollOption.java
@@ -0,0 +1,90 @@
+package com.team11.hackernews.api;
+
+public class PollOption {
+    private Long mId;
+    private String mBy;
+    private long mTime;
+    private String mText;
+    private long mParent;
+    private Long mScore;
+
+    private PollOption(Builder builder) {
+        mId = builder.mId;
+        mBy = builder.mBy;
+        mTime = builder.mTime;
+        mText = builder.mText;
+        mParent = builder.mParent;
+        mScore = builder.mScore;
+    }
+
+    public Long getId() {
+        return mId;
+    }
+
+    public String getBy() {
+        return mBy;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public long getParent() {
+        return mParent;
+    }
+
+    public Long getScore() {
+        return mScore;
+    }
+
+    public static final class Builder {
+        private Long mId;
+        private String mBy;
+        private long mTime;
+        private String mText;
+        private long mParent;
+        private Long mScore;
+
+        public Builder() {
+        }
+
+        public Builder id(Long id) {
+            this.mId = id;
+            return this;
+        }
+
+        public Builder by(String by) {
+            this.mBy = by;
+            return this;
+        }
+
+        public Builder time(long time) {
+            this.mTime = time;
+            return this;
+        }
+
+        public Builder text(String text) {
+            this.mText = text;
+            return this;
+        }
+
+        public Builder parent(long parent) {
+            this.mParent = parent;
+            return this;
+        }
+
+        public Builder score(Long score) {
+            this.mScore = score;
+            return this;
+        }
+
+        public PollOption build() {
+            return new PollOption(this);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/team11/hackernews/api/accessors/PollOptionAccessor.java
@@ -0,0 +1,99 @@
+package com.team11.hackernews.api.accessors;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.team11.hackernews.api.HackerNewsAPI;
+import com.team11.hackernews.api.PollOption;
+import com.team11.hackernews.api.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class PollOptionAccessor extends Accessor{
+
+    public interface GetPollOptionsCallbacks {
+        public void onSuccess(List<PollOption> pollOpts);
+
+        public void onError();
+    }
+
+    public void getPollOptions(final List<Long> ids, final GetPollOptionsCallbacks callbacks) {
+        final List<PollOption> pollOpts = new ArrayList<PollOption>();
+
+        if (ids.size() == 0) {
+            callbacks.onSuccess(pollOpts);
+            return;
+        }
+
+        for (final long id : ids) {
+            getPollOption(id, new GetPollOptionCallbacks() {
+                @Override
+                public void onSuccess(PollOption pollOpt) {
+                    if (mCancelPendingCallbacks) {
+                        return;
+                    }
+                    pollOpts.add(pollOpt);
+
+                    if (pollOpts.size() == ids.size()) {
+                        callbacks.onSuccess(pollOpts);
+                    }
+                }
+
+                @Override
+                public void onError() {
+                    if (mCancelPendingCallbacks) {
+                        return;
+                    }
+                    callbacks.onError();
+                }
+            });
+        }
+    }
+
+    public interface GetPollOptionCallbacks {
+        public void onSuccess(PollOption pollOpt);
+        public void onError();
+    }
+
+    private void getPollOption(long id, final GetPollOptionCallbacks callbacks) {
+        Utils.getFirebaseInstance().child(HackerNewsAPI.ITEM + "/" + id).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (mCancelPendingCallbacks) {
+                    return;
+                }
+
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+                Utils.ItemType itemType = Utils.getItemTypeFromString(map.get("type").toString());
+                if (itemType != Utils.ItemType.Pollopt) {
+                    callbacks.onError();
+                }
+
+                PollOption pollOption = new PollOption.Builder()
+                        .by((String) map.get("by"))
+                        .id((Long) map.get("id"))
+                        .parent((Long) map.get("parent"))
+                        .score((Long) map.get("score"))
+                        .text((String) map.get("text"))
+                        .time((Long) map.get("time"))
+                        .build();
+
+                callbacks.onSuccess(pollOption);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                if (mCancelPendingCallbacks) {
+                    return;
+                }
+
+                callbacks.onError();
+            }
+        });
+    }
+
+
+}
