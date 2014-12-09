1f151eb6cc81fcc2ab637b205b844c768a2fe437
Rob Game <rgameuk@gmail.com>
2014-12-09 12:12:08 +0000

    Changed strings on sidebar

--- app/src/main/java/com/team11/hackernews/MainBase.java
+++ app/src/main/java/com/team11/hackernews/MainBase.java
@@ -57,13 +57,16 @@ public abstract class MainBase extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = getString(R.string.title_topStories);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = getString(R.string.title_users);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = getString(R.string.title_subscribedStories);
+                break;
+            case 4:
+                mTitle = getString(R.string.action_settings);
                 break;
         }
     }
--- app/src/main/java/com/team11/hackernews/NavigationDrawerFragment.java
+++ app/src/main/java/com/team11/hackernews/NavigationDrawerFragment.java
@@ -101,9 +101,10 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.title_topStories),
+                        getString(R.string.title_users),
+                        getString(R.string.title_subscribedStories),
+                        getString(R.string.action_settings),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
